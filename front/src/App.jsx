64ba703dbcc37f3cfa3e4487c1d9a5eae65b0e2c
ycenta <thorbenliya@gmail.com>
2023-11-06 16:59:59 +0100

    add material ui and search bar

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -6,6 +6,8 @@ import Button from './components/Button'
 import SearchBar from './components/SearchBar'
 import Receip from './components/Receip'
 import ReceipList from './components/ReceipList'
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
 
 
 function App() {
@@ -13,12 +15,25 @@ function App() {
   const [receips, setReceips] = useState([])
   const ingredientsRandom = [{text: "testingredient1"}, {text: "testingredient2"}, {text: "testingredient3"}]
 
+  const top100Films = [
+    { title: 'The Shawshank Redemption', year: 1994 },
+    { title: 'The Godfather', year: 1972 },
+  ];
+
+
   return (
     <>
       <div className="mainTitle">
         <h1>Ai Cooking</h1>
       </div>
     <SearchBar/> 
+    <Autocomplete
+        id="free-solo-demo"
+        freeSolo
+        options={top100Films.map((option) => option.title)}
+        renderInput={(params) => <TextField {...params} label="freeSolo" />}
+      />
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}

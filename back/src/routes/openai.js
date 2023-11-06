5f884cf08ce91abe7087100b107d734f91a8f4bd
ycenta <thorbenliya@gmail.com>
2023-11-06 16:35:27 +0100

    add needed enpoints to get similar recipies, or courses from recepies

--- back/src/routes/openai.js
+++ back/src/routes/openai.js
@@ -12,11 +12,12 @@ router.get('/', (req, res) => {
   res.send('open api');
 });
 
-router.post('/sendmessage', async (req, res) => {
+router.post('/accompagnement', async (req, res) => {
+    //Route pour proposer des accompagnements intelligents aux recettes comme du vin,des desserts ou des fromages
 
   console.log(req.body);
 
-  const message = req.body.destination;
+  const recette = req.body.destination;
 
   try {
       const completions = await openai.chat.completions.create({
@@ -24,11 +25,11 @@ router.post('/sendmessage', async (req, res) => {
           messages: [
               {
                   role: "system",
-                  content: "Tu es chef patissier"
+                  content: "Tu es chef de cuisine, ton role est de me proposer un accompagnement intelligent aux recettes que je te fournis, comme du vin, des desserts ou des fromages"
               },
               {
                   role: "user",
-                  content: message
+                  content: recette
               }
           ]
       });
@@ -42,12 +43,12 @@ router.post('/sendmessage', async (req, res) => {
 });
 
 
+router.post('/get-courses', async (req, res) => {
 
-
-router.post('/moreinformation', async (req, res) => {
+    //route pour génerer des courses selon les recettes qu'on fourni
 
   console.log(req.body);
-  const message = req.body.moredestination;
+  const recette = req.body.recette;
 
   try {
       const completions = await openai.chat.completions.create({
@@ -55,11 +56,11 @@ router.post('/moreinformation', async (req, res) => {
           messages: [
               {
                   role: "system",
-                  content: "Bonjour, quand je vais te donner un pays, tu vas devoir me donner plus d'information sur celui-ci, comme le climat, la population, la monnaie"
+                  content: "Bonjour, tu es expert en nourriture, lorsque je vais te donner une recette de cuisine, tu devra me génerer une liste de course intélligente"
               },
               {
                   role: "user",
-                  content: message
+                  content: recette
               }
           ]
       });
@@ -69,6 +70,34 @@ router.post('/moreinformation', async (req, res) => {
       console.error(error);
       res.status(500).send('Internal Server Error');
   }
+
+});
+
+
+router.post('/get-similar-recettes', async (req, res) => {
+    //route pour génerer des recettes similaires à celle fournie
+
+    console.log(req.body);
+    const recette = req.body.recette;
+
+    try{
+        const completions = await openai.chat.completions.create({
+            model: "gpt-3.5-turbo",
+            messages: [
+                {
+                    role: "system",
+                    content:"Bonjour, tu es export en nourriture, tu vas devoir me donner au moins une recette similaire (en format json) à celle que je t'ai fournie"
+                },
+                {
+                    role: "user",
+                    content: recette
+                }
+            ]
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
 
 });
 

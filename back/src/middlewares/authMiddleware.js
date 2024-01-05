111ac42c75b4bbf6e71d3003094b2bdb35ed0bc6
Samuel GUENIER <guenier.samuel@gmail.com>
2024-01-05 14:18:50 +0100

    Adding middleware for auth in back

---
+++ back/src/middlewares/authMiddleware.js
@@ -0,0 +1,18 @@
+import jwt from 'jsonwebtoken';
+
+export default function(req, res, next) {
+  const token = req.headers['authorization'];
+
+  if (!token) {
+    return res.status(403).send({ auth: false, message: 'No token provided.' });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, function(err, decoded) {
+    if (err) {
+      return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+    }
+
+    req.userId = decoded.id;
+    next();
+  });
+}

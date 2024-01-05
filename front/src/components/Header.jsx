1efcb431dff850bc0c8fdccd3008686fa1f8fba3
Samuel GUENIER <guenier.samuel@gmail.com>
2024-01-05 13:46:01 +0100

    Adding logout route, + better login logic

--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -1,12 +1,23 @@
+import { useContext } from 'react';
+import { AuthContext } from '../contexts/api/AuthContext';
 import styles from './Header.module.scss';
 
 const Header = () => {
+  const { isAuthenticated, logout } = useContext(AuthContext);
+
   return (
     <header className={styles.Header}>
         <a href="/" className={styles.HeaderLogo}><img src='/src/assets/logo-icon.svg' /></a>
         <div className={styles.HeaderActions}>
-            <a href="/register" className={styles.HeaderActionsBtn}>Register</a>
-            <a href="/login" className={styles.HeaderActionsBtn}>Login</a>
+            {!isAuthenticated && (
+              <>
+                <a href="/register" className={styles.HeaderActionsBtn}>Register</a>
+                <a href="/login" className={styles.HeaderActionsBtn}>Login</a>
+              </>
+            )}
+            {isAuthenticated && (
+              <button onClick={logout} className={styles.HeaderActionsBtn}>Logout</button>
+            )}
         </div>
     </header>
   );
--- front/src/contexts/api/AuthContext.jsx
+++ front/src/contexts/api/AuthContext.jsx
@@ -1,26 +1,35 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 import axios from '../../axios';
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState(null);
+  const [isAuthenticated, setIsAuthenticated] = useState(Boolean(token));
+
+  useEffect(() => {
+    const storedToken = localStorage.getItem('token');
+    if (storedToken) {
+      setToken(storedToken);
+      setIsAuthenticated(true);
+    }
+  }, []);
 
   const login = async (email, password) => {
-    const response = await axios.post('/user/login', 
-    { 
-        email, 
-        password
-    });
+    const response = await axios.post('/user/login', { email, password });
+    localStorage.setItem('token', response.data.token);
     setToken(response.data.token);
+    setIsAuthenticated(true);
   };
 
   const logout = () => {
     setToken(null);
+    localStorage.removeItem('token');
+    setIsAuthenticated(false);
   };
 
   return (
-    <AuthContext.Provider value={{ token, login, logout }}>
+    <AuthContext.Provider value={{ token, login, logout, isAuthenticated, setIsAuthenticated }}>
       {children}
     </AuthContext.Provider>
   );

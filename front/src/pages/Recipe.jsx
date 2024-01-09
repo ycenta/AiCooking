a33425fc09011c78cc9f9de84f1d2a0cf49a5039
Samuel GUENIER <guenier.samuel@gmail.com>
2024-01-09 19:03:17 +0100

    kinda better style and organization in page

--- front/src/pages/Recipe.jsx
+++ front/src/pages/Recipe.jsx
@@ -2,6 +2,7 @@ import { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { RecipesContext } from '../contexts/api/RecipesContext.jsx';
 import { OpenAiContext } from  '../contexts/api/OpenAiContext.jsx';
+import styles from '../styles/Recipe.module.scss';
 
 function Recipe() {
     const { id } = useParams();
@@ -29,28 +30,29 @@ function Recipe() {
                 <div>
                     <h1>{recipe.title}</h1>
                     <div>{recipe.description}</div>
-                    <div>
-                        <h2>Ingredients</h2>
-                        <ul>
-                            {Object.entries(JSON.parse(recipe.ingredients)).map(([key, value]) => (
-                                <li key={key}>{value} {key}</li>
-                            ))}
-                        </ul>
+                    <div className={styles.bodyRecipe}>
+                        <div className={styles.bodyRecipeIngredients}>
+                            <h2>Ingredients</h2>
+                            <ul>
+                                {Object.entries(JSON.parse(recipe.ingredients)).map(([key, value]) => (
+                                    <li key={key}>{value} {key}</li>
+                                ))}
+                            </ul>
+                        </div>
+                        <div>
+                            <h2>Etapes : </h2>
+                            <ul>
+                                {Object.entries(JSON.parse(recipe.steps)).map(([key, value]) => (
+                                    <li key={key}>{parseInt(key)+1}) &nbsp; {value}</li>
+                                ))}
+                            </ul>
+                        </div>
                     </div>
                     <div>
-                        <h2>Etapes : </h2>
-                        <ul>
-                            {Object.entries(JSON.parse(recipe.steps)).map(([key, value]) => (
-                                <li key={key}>{parseInt(key)+1}) &nbsp; {value}</li>
-                            ))}
-                        </ul>
-                    </div>
-                    <div>
-                        <button onClick={() => postSimilar({ "recette": recipe.title })}>Trouver des recettes similaires</button>
                         {similarRecipes && (
                             <div>
                             <h2>Recettes Similaires : </h2>
-                            <ul>
+                            <ul className={styles.listSimilar}>
                                 {similarRecipes && Object.keys(similarRecipes).length > 0 && (
                                     JSON.parse(similarRecipes).map((recipe, index) => (
                                         <li key={index}>{recipe}</li>
@@ -59,6 +61,7 @@ function Recipe() {
                             </ul>
                             </div>
                         )}
+                        <button onClick={() => postSimilar({ "recette": recipe.title })}>Trouver des recettes similaires</button>
                     </div>
                 </div>
                 
